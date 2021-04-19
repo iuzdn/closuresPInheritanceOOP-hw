ac00b642c863a154fde57ffb001f050dead4a2ff
izadnipru <iulian.zadnipru@endava.com>
2021-04-19 14:02:56 +0300

    Correction according to mentor recommendations

--- homework2.js
+++ homework2.js
@@ -12,24 +12,19 @@ const e = new HtmlElement();
 e.click();
 e.focus();
 
-// the value for "items" is optional. What if I decide to pass an initial value like so: new HtmlSelectElement([1, 2, 3])?
-function HtmlSelectElement() {
-  HtmlElement.call(this);
-  this.items = {};
+function HtmlSelectElement(items = []) {
+  this.items = items;
+
+  this.addItem = function (item) {
+    this.items.push(item);
+  };
+
+  this.removeItem = function (item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  };
 }
-HtmlSelectElement.prototype = Object.create(HtmlElement.prototype);
-HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
-// It would've been better to have the implementation for adding and removing an item to the items list also.
-// And by setting these methods on the prototype, you will not be able to access items, because it's on another level
-// You could call addItem and removeItem, but you cannot access items property.
-HtmlSelectElement.prototype.addItem = function () {
-  console.log("Add new Item");
-};
-
-HtmlSelectElement.prototype.removeItem = function () {
-  console.log("Remove item");
-};
+HtmlSelectElement.prototype = new HtmlElement();
 
 const d = new HtmlSelectElement();
 d.click();
